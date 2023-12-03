0538a0d3c31c1db93f7febb5918322a0d8ee2908
Linus Horn <linushorn@gmx.net>
2023-12-03 15:03:54 +0100

    Day 2 Part 2 Python
    Pretty much the same as Part 1

---
+++ Day_2/2_2.py
@@ -0,0 +1,60 @@
+#Advent of Code 2023 Day 2 Part 1
+
+import os
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+with open("./data.txt", "r") as f:
+    input_lines = f.readlines()
+input_lines = [line.replace("\n", "") for line in input_lines]
+
+limits = {
+    "red": 12,
+    "green": 13,
+    "blue": 14
+}
+
+class Game:
+    def __init__(self, game_str):
+        self.id: int = int(game_str[5:game_str.index(":")])
+        self.max_values = self.calculate_subsets(game_str)
+        
+    
+    def calculate_subsets(self, game_str):
+        out_dict = {
+            "blue": 0,
+            "green": 0,
+            "red": 0
+        }
+        
+        temp_str  = game_str.split(": ")[-1]
+        sets = temp_str.split("; ")
+        for s in sets:
+            for elem in s.split(", "):
+                color = ""
+                for c in out_dict.keys():
+                    if elem.endswith(c):
+                        color = c
+                
+                val_old = out_dict[color]
+                val_new = int(elem.split(" ")[0])
+                out_dict[color] = max([val_new, val_old])
+        
+        return out_dict
+                
+
+
+games = []
+for line in input_lines:
+    games.append(Game(line))
+
+s = 0
+
+for game in games:
+    temp_sum = 1
+    for val in game.max_values.values():
+        temp_sum *= val
+    
+    s += temp_sum
+
+print(s)
